886f9521b9ce22ae41a48af7016bdd316b50e4fc
Suraj <sgshining@gmail.com>
2016-04-01 19:53:30 +0530

    Added predict fun

--- scripts/algorithm.py
+++ scripts/algorithm.py
@@ -1,6 +1,7 @@
 from load_data import load_data
 from numpy.random import standard_normal as randn
 import numpy as np
+from scipy import optimize
 
 class algorithm():
 
@@ -13,11 +14,13 @@ class algorithm():
 		self.theta = randn((self.users, self.features))
 		self.X = randn((self.movies, self.features))
 
-	def cost_function(self, Y, r, lamda):
-		H = (self.X.dot(self.theta.T) - Y)*r
-		J = (1.0/2)*np.sum(np.sum(H**2)) + (1.0/2)*lamda*np.sum(np.sum(self.theta**2)) + (1.0/2)*lamda*np.sum(np.sum(self.X**2))
-		X_grad = H.dot(self.theta) + lamda*self.X
-		theta_grad = self.X.T.dot(H) + lamda*self.theta
+	def cost_function(self, param,Y, r, lamda):
+		X=reshape(param(1:self.movies*self.features), self.movies, self.features)
+		theta = reshape(params(self.movies*self.features+1:end), self.users, self.features)		
+		H = (X.dot(theta.T) - Y)*r
+		J = (1.0/2)*np.sum(np.sum(H**2)) + (1.0/2)*lamda*np.sum(np.sum(theta**2)) + (1.0/2)*lamda*np.sum(np.sum(X**2))
+		X_grad = H.dot(theta) + lamda*X
+		theta_grad = X.T.dot(H) + lamda*theta
 
 	def normalize(self, Y, r):
 		(m, n) = Y.shape
@@ -30,9 +33,22 @@ class algorithm():
 			Ynorm[i][idx] = Y[i][idx]-Ymean[i]
 		return (Ynorm, Ymean)
 
+	def predict(self,Y,r,lamda):
+		param = np.append(self.X.flatten(), self.theta.flatten())
+		res1 = optimize.fmin_cg(cost_function, param, args=(Y, r,lamda), maxiter=100, disp=True)
+
+		X=reshape(res1(1:self.movies*self.features), self.movies, self.features)
+		theta = reshape(res1(self.movies*self.features+1:end), self.users, self.features)
+
+		pred=X.dot(theta.T);
+		predt=pred*(1-r);
+
+		return np.argsort(predt,0)[::-1][:20];
 
 if __name__=="__main__":
 	obj = algorithm(10, 10, 10)
 	Y = np.array([[1,2,3],[4,5,6],[7,8,9]])
 	r = np.array([[0, 1, 0], [1, 0, 1], [1, 1, 1]])
+	lamda=10
 	print obj.normalize(Y, r)
+	print predict(Y,r,lamda)
